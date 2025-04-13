c6a3f781e97ce1a859bded878153bc6a1f7e6157
juan felipe <jf@duartte.co>
2025-04-13 16:46:28 +0200

    adding space so it's easy to read

--- main.go
+++ main.go
@@ -10,9 +10,10 @@ func main(){
 	http.HandleFunc("/hello", helloHandler)
 	http.HandleFunc("/task", taskHandler)
 	http.HandleFunc("/tasks", tasksHandler)
-	
+
 	fmt.Println("Server is running on port 8080")
 	err := http.ListenAndServe(":8080", nil)
+
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 	}
