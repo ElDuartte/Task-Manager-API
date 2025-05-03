9ca43eb2ae3e496a6a76d924cebea5c53f8e5897
ElDuartte <jf@duartte.co>
2025-05-03 20:09:04 +0200

    wip adding task types

--- task.go
+++ task.go
@@ -1,9 +1,18 @@
 package main
+import "time"
 
 type Task struct {
     ID        int       `json:"id"`
     Title     string    `json:"title"`
     Completed bool      `json:"completed"`
-    Deleted bool        `json:"deleted"`
+    Deleted   bool      `json:"deleted"`
+    Edited    time.Time    `json:"time"`
+    CreatedTime time.Time 
+    AssignedToID int
+    
 }
+
+# Creatorid
+# ProjectID -- needs to be an array in case it belongs to multiple projects at the same time
+# AssignedToID -- needs to be an array in case it's for multiple devs
 
