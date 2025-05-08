0c304b402a3587966e6d3fda050a3be338cf9b0c
juan felipe <jf@duartte.co>
2025-05-08 13:03:50 +0200

    adding the types we will probably need

--- task.go
+++ task.go
@@ -1,18 +1,19 @@
 package main
+
 import "time"
 
 type Task struct {
-    ID        int       `json:"id"`
-    Title     string    `json:"title"`
-    Completed bool      `json:"completed"`
-    Deleted   bool      `json:"deleted"`
+    ID        int          `json:"id"`
+    CreatorID int          `json:"creatorId"`
+		ProjectID []int        `json:"projectId"`
+		AssignedToID []int     `json:"assignedToId"`
+    Title     string       `json:"title"`
+    Completed bool         `json:"completed"`
+    Deleted   bool         `json:"deleted"`
     Edited    time.Time    `json:"time"`
-    CreatedTime time.Time 
-    AssignedToID int
-    
+		CreatedTime time.Time  `json:"createdTime"`
 }
 
-# Creatorid
-# ProjectID -- needs to be an array in case it belongs to multiple projects at the same time
-# AssignedToID -- needs to be an array in case it's for multiple devs
+// ProjectID []int is the syntax to declare an array of integers
+// AssignedToID
 
