4d3890eeb56195f24634422d0703fdd6c1cee90c
juan felipe <jf@duartte.co>
2025-05-08 13:43:14 +0200

    fixing type logic error and making it look goooood

--- task.go
+++ task.go
@@ -3,15 +3,15 @@ package main
 import "time"
 
 type Task struct {
-    ID        int          `json:"id"`
-    CreatorID int          `json:"creatorId"`
-		ProjectID []int        `json:"projectId,omitempty"`
-		AssignedToID []int     `json:"assignedToId,omitempty"`
-    Title     string       `json:"title"`
-    Status   []string      `json:"status"`
-    Deleted   bool         `json:"deleted"`
-    Edited    time.Time    `json:"time"`
-		CreatedTime time.Time  `json:"createdTime"`
+    ID           int        `json:"id"`
+    CreatorID    int        `json:"creatorId"`
+		ProjectID    []int      `json:"projectId,omitempty"`
+		AssignedToID []int      `json:"assignedToId,omitempty"`
+    Title        string     `json:"title"`
+    Status       string     `json:"status"`
+    Deleted      bool       `json:"deleted"`
+    Edited       time.Time  `json:"time"`
+		CreatedTime  time.Time  `json:"createdTime"`
 }
 
 // ProjectID []int is the syntax to declare an array of integers
