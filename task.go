a895f16bebdfb630f5dab9df8c00f3468c0f9fc2
juan felipe <jf@duartte.co>
2025-05-10 23:24:47 +0200

    typos in the struct

--- task.go
+++ task.go
@@ -6,14 +6,14 @@ type Task struct {
     ID           int        `json:"id"`
     CreatorID    int        `json:"creatorId"`
     ProjectID    []int      `json:"projectId,omitempty"`
-	  AssignedToID []int      `json:"assignedToId,omitempty"`
+    AssignedToID []int      `json:"assignedToId,omitempty"`
     Title        string     `json:"title"`
     Description  string     `json:"description"`
     Status       string     `json:"status"`
     Deleted      bool       `json:"deleted"`
-	  Deadline     time.Time  `json:"deadlineTime"`
+    Deadline     time.Time  `json:"deadlineTime"`
     Edited       time.Time  `json:"editedTime"`
-	  CreatedTime  time.Time  `json:"createdTime"`
+    CreatedTime  time.Time  `json:"createdTime"`
 }
 
 // ProjectID []int is the syntax to declare an array of integers
