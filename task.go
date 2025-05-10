fbbfbf34cfb3bd994b63ea6f122de5b0b22b9e64
juan felipe <jf@duartte.co>
2025-05-10 22:33:34 +0200

    adding deadline to the struct

--- task.go
+++ task.go
@@ -5,14 +5,15 @@ import "time"
 type Task struct {
     ID           int        `json:"id"`
     CreatorID    int        `json:"creatorId"`
-		ProjectID    []int      `json:"projectId,omitempty"`
-		AssignedToID []int      `json:"assignedToId,omitempty"`
+    ProjectID    []int      `json:"projectId,omitempty"`
+	  AssignedToID []int      `json:"assignedToId,omitempty"`
     Title        string     `json:"title"`
     Description  string     `json:"description"`
     Status       string     `json:"status"`
     Deleted      bool       `json:"deleted"`
+	  Deadline     time.Time  `json:"time"`
     Edited       time.Time  `json:"time"`
-		CreatedTime  time.Time  `json:"createdTime"`
+	  CreatedTime  time.Time  `json:"createdTime"`
 }
 
 // ProjectID []int is the syntax to declare an array of integers
